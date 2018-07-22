26019f99a5f3bf0216c6a4a9391d2255d486c9d5
shripriyaprabhakar <37489016+shripriyaprabhakar@users.noreply.github.com>
2018-07-21 21:09:36 -0700

    changes per feedback. Fixed indentations. Changed function names

--- database/index.js
+++ database/index.js
@@ -1,60 +1,71 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
 const faker = require('faker');
 const Schema = mongoose.schema; // added this line
 mongoose.connect('mongodb://localhost/fetcher');
 
 let shopsAvalAtArr = ['COSTCO', 'Wallmart', 'Target', 'FRYs Electronics', 'AdoramaCamera'];
 
-// var imageFunc = function () {
-//   var imagePath = 
-// }
 
-let idFunc = function () {
+
+let generateIdFunc = function () {
   return Math.floor(Math.random() * Math.floor(5000));  
 };
 
-let priceFunc = function () {
+let generatePriceFunc = function () {
    var a =  Math.floor(Math.random() * Math.floor(5000));
   return faker.commerce.price(.10,a,2,"$");
 }; 
 
-let deliveryCostFunc = function () {
+let generateDeliveryCostFunc = function () {
   return 'Free delivery'; 
 };
 
-let randomDate = function (start, end) {
+let generateRandomDate = function (start, end) {
   for (var i =0 ; i < 100; i++) {
     var dateNew = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     return dateNew;
   }
 };   
 
-let descFunc = function () {
+let generateDescFunc = function () {
   return (faker.commerce.productName());
 };
 
-let ratingNum = function() {
+let generateRatingNum = function() {
   return Math.floor(Math.random() * Math.floor(1000));
 };
 
-let shopSelect = function () {
+let generateShopSelect = function () {
   var max = shopsAvalAtArr.length
   var indexNum = Math.floor(Math.random(0) * Math.floor(max));
   return shopsAvalAtArr[indexNum];
 }
 
+var imgPathArr = [];
+let generateImagePath = function () {
+  for (var i = 0; i < 90; i++) {
+     // var img_var = "hrsf99" + "\/" + "expressal-similar-Items-service" + "\/" + "images" + "\/" + "image-[" + i + "].png"; 
+     var img_var =   "\/" + "images" + "\/" + "image-[" + i + "].png"; 
+     imgPathArr.push(img_var);
+  }
+     return imgPathArr;
+}
+generateImagePath();   
+ 
 let itemList = [];
 
 let populateData = function () {
   for (var i =0 ; i < 100; i++) {
     var item = {
-      id: idFunc(),
-      price: priceFunc(),
-      deliveryCost: deliveryCostFunc(),
-      dateOfDelivery: randomDate(new Date(2012, 0, 1), new Date()),
-      desc: descFunc(),
-      rating: ratingNum(),
-      shopsAvalAt: shopSelect()
+      id: generateIdFunc(),
+      price: generatePriceFunc(),
+      deliveryCost: generateDeliveryCostFunc(),
+      dateOfDelivery: generateRandomDate(new Date(2012, 0, 1), new Date()),
+      desc: generateDescFunc(),
+      rating: generateRatingNum(),
+      shopsAvalAt: generateShopSelect(),
+      imgPath: imgPathArr[i]
     }
     itemList.push(item);
   }
@@ -68,13 +79,15 @@ let prodSchema = mongoose.Schema({
   dateOfDelivery: String,
   desc: String,
   rating: Number,
-  shopsAvalAt: String
+  shopsAvalAt: String,
+  img: { data: Buffer, contentType: String }
+  // img: String
 });
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-console.log('we are connected!');
+db.once('open', function () {
+  console.log('we are connected!');
 });
 
 let Prod = mongoose.model('Prod', prodSchema);
@@ -89,14 +102,17 @@ let saveList = (itemList, cb) => {
     dateOfDelivery: itemList[i].dateOfDelivery,
     desc: itemList[i].desc,
     rating: itemList[i].rating,
-    shopsAvalAt: itemList[i].shopsAvalAt
-    // img: { data: Buffer, contentType: String }
+    shopsAvalAt: itemList[i].shopsAvalAt,
+    img: {data: fs.readFileSync(itemList[i].imgPath),
+          contentType : 'image/png' }
     });
 
-    newProd.save(cb);
   }
 
-};
+  newProd.save(cb);
+} 
+
+
 
 saveList(itemList,  (err, product) => {
   if (err) {
@@ -110,5 +126,77 @@ saveList(itemList,  (err, product) => {
 
 module.exports.saveList = saveList;
 module.exports.itemList = itemList;
-module.exports.priceFunc = priceFunc;
-module.exports.shopSelect = shopSelect;
+
+
+
+
+
+////////////////////////////////////
+/*
+
+var express = require('express');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var imgPath = '/path/yourimage.png';
+
+mongoose.connect('localhost', 'testing_storeImg');
+
+var schema = new Schema({
+    img: { data: Buffer, contentType: String }
+});
+
+var A = mongoose.model('A', schema);
+
+mongoose.connection.on('open', function () {
+  console.error('mongo is open');
+
+  A.remove(function (err) {
+    if (err) throw err;
+
+    console.error('removed old docs');
+
+    // store an img in binary in mongo
+    var a = new A;
+    a.img.data = fs.readFileSync(imgPath);
+    a.img.contentType = 'image/png';
+    a.save(function (err, a) {
+      if (err) throw err;
+
+      console.error('saved img to mongo');
+
+      // start a demo server
+      var server = express.createServer();
+      server.get('/', function (req, res, next) {
+        A.findById(a, function (err, doc) {
+          if (err) return next(err);
+          res.contentType(doc.img.contentType);
+          res.send(doc.img.data);
+        });
+      });
+
+      server.on('close', function () {
+        console.error('dropping db');
+        mongoose.connection.db.dropDatabase(function () {
+          console.error('closing db connection');
+          mongoose.connection.close();
+        });
+      });
+
+      server.listen(3333, function (err) {
+        var address = server.address();
+        console.error('server listening on http://%s:%d', address.address, address.port);
+        console.error('press CTRL+C to exit');
+      });
+
+      process.on('SIGINT', function () {
+        server.close();
+      });
+    });
+  });
+
+});
+
+
+*/
