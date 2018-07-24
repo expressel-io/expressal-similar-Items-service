a6e65bcf1529565238fbe59c1feed824444727f3
shripriyaprabhakar <37489016+shripriyaprabhakar@users.noreply.github.com>
2018-07-24 16:43:16 -0700

    Started adding css styles to resemble original webpage

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -1,35 +1,39 @@
 import React, { Component } from 'react'
 // import $ from 'jquery';
 import axios from 'axios';
-// import Sim from './Sim';
+import '../styles/Items.css';
 
 export default class Items extends Component { 
   render() {
     return (
-      <div className="items-view">
-        <div className = "price-view">
+      <div className="Items-product-info-container">
+        <div className = "Items-price-column">
           <p  onClick={this.handleClick}>
             {this.props.items.price}
           </p>
         </div>
-        <div className = "deliveryCost-view">
-          <p  onClick={this.handleClick}>
-            {this.props.items.deliveryCost}
-          </p>
+        <div className = "Items-product-info-container">
+          <div className = "Items-subtitle">
+            <p  onClick={this.handleClick}>
+              {this.props.items.deliveryCost}
+            </p>
+          </div>
         </div>
-        <div className = "dateOfDelivery-view">
-          <p  onClick={this.handleClick}>
-            {this.props.items.dateOfDelivery}
-          </p>
+        <div className = "Items-product-Info-container">
+          <div className = "Items-subtitle-days">
+            <p  onClick={this.handleClick}>
+              {this.props.items.dateOfDelivery}
+            </p>
+          </div>
         </div>
         <br /> 
-        <div className = "description-view">
+        <div className = "Items-product-title">
           <p  onClick={this.handleClick}>
             {this.props.items.desc}
           </p>
         </div>
         <br /> 
-        <div className = "ratings-view">
+        <div className = "Items-review-Count">
           <p  onClick={this.handleClick}>
             {this.props.items.rating}
           </p>
